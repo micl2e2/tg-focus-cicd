0634ed728b77f7d97403661daee1bbde0592c45a
Michael Lee <micl2e2@proton.me>
2023-11-17 10:41:34 +0800

    run build-tgfocus-gittag-x64-linux-musl

--- before-action.bash
+++ before-action.bash
@@ -2,7 +2,8 @@ run_what="$1"
 
 if [[ $run_what =~ ^run\ build-[a-z0-9]+ ]]
 then
-    echo "$run_what $(TZ=UTC date)"
+    echo "$run_what $(TZ=UTC date)" >>actiontrigger
+    echo '[INFO] done'
 else
     echo '[INFO] bad input'
 fi
